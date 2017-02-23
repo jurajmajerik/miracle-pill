a160641befb40a079f8a76e98eed7eac3a8fcaa0
jurajmajerik <juro.majerik@gmail.com>
2017-02-23 02:43:29 +0100

    created a string "state"

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     let states = ["Alaska", "Arkansas", "Alabama", "California", "Maine", "New York"]
     
+    let string = "state" 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.statePicker.dataSource = self
